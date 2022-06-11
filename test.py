551ef071af4a9c28c396e76870d41838ad75a6ba
Udichei <syanova_e@mail.ru>
2022-06-11 18:13:09 +0200

    add updating system

--- test.py
+++ test.py
@@ -2,28 +2,41 @@ from time import sleep
 import psutil
 from tkinter import *
 
-
 temperatures = psutil.sensors_temperatures()
 root = Tk()
 
-# for el in temperatures:
-#     print(el)
-#     perv = temperatures[el]
-#     print(type(perv))
-#     print(perv[0])
-#     print(type(perv[0]))
-#     print('\n')
-#     # print(perv[1])
-#     # print('\n')
-#     #print(perv)
 
-print(temperatures)
+new_text = ''
+sec_col  = ''
+third_col= ''
+fo_col   = ''
+fif_col  = ''
 
-new_text = 'name\n'
-sec_col  = 'label\n'
-third_col= 'current\n'
-fo_col   = 'high\n'
-fif_col  = 'critical\n'
+####### update columns #######
+def update():
+    temperatures = psutil.sensors_temperatures()
+    third_col= ''
+    fo_col   = ''
+    fif_col  = ''
+
+    ######## third column ########
+    for elm in temperatures:
+        x = 0
+        for x in range(len(temperatures[elm])):
+            third_col += str(temperatures[elm][x].current) + '\n'
+            fo_col += str(temperatures[elm][x].high) + '\n'
+            fif_col += str(temperatures[elm][x].critical) + '\n'
+
+######## lables and titles ########
+    lbl3 = Label(root, text = third_col,  font=("Arial Bold", 12))
+    lbl3.grid(column=2, row=1)
+
+    lbl4 = Label(root, text = fo_col,  font=("Arial Bold", 12))
+    lbl4.grid(column=3, row=1)
+
+    lbl5 = Label(root, text = fif_col,  font=("Arial Bold", 12))
+    lbl5.grid(column=4, row=1)
+    root.after(1000, update)
 
 ######## first column ########
 for elm in temperatures:
@@ -35,55 +48,32 @@ for elm in temperatures:
     for x in range(len(temperatures[elm])):
         sec_col += str(temperatures[elm][x].label) + '\n'
 
-######## third column ########
-for elm in temperatures:
-    x = 0
-    for x in range(len(temperatures[elm])):
-        third_col += str(temperatures[elm][x].current) + '\n'
-
-######## fourt column ########
-for elm in temperatures:
-    x = 0
-    for x in range(len(temperatures[elm])):
-        fo_col += str(temperatures[elm][x].high) + '\n'
-
-######## fifth column ########
-for elm in temperatures:
-    x = 0
-    for x in range(len(temperatures[elm])):
-        fif_col += str(temperatures[elm][x].critical) + '\n'
-
-# for elm in temperatures:
-#     print(elm)
-
-
-# for elm in temperatures:
-#     sec_col += str(temperatures[elm][0].current) + '\n'
-
-text = psutil.sensors_temperatures()
-#print(text)
-# dlin = len(text)
-# print(dlin)
-
 
 ######## lables and titles ########
 root.title("System temperature")
 root.geometry("600x350")
 
-lbl = Label(root, text = new_text)
-lbl.grid(column=0, row=0)
+title1 = Label(root, text = "  Name  ",  font=("Arial Bold", 18))
+title1.grid(column=0,row=0)
 
-lbl2 = Label(root, text = sec_col)
-lbl2.grid(column=2, row=0)
+title2 = Label(root, text = "  Label  ",  font=("Arial Bold", 18))
+title2.grid(column=1,row=0)
 
-lbl3 = Label(root, text = third_col)
-lbl3.grid(column=3, row=0)
+title3 = Label(root, text = "  Current  ",  font=("Arial Bold", 18))
+title3.grid(column=2,row=0)
 
-lbl4 = Label(root, text = fo_col)
-lbl4.grid(column=4, row=0)
+title2 = Label(root, text = "  High  ",  font=("Arial Bold", 18))
+title2.grid(column=3,row=0)
 
-lbl5 = Label(root, text = fif_col)
-lbl5.grid(column=5, row=0)
+title2 = Label(root, text = "  Critical  ",  font=("Arial Bold", 18))
+title2.grid(column=4,row=0)
 
-######## wndow start ########
+lbl = Label(root, text = new_text,  font=("Arial Bold", 12))
+lbl.grid(column=0, row=1)
+
+lbl2 = Label(root, text = sec_col,  font=("Arial Bold", 12))
+lbl2.grid(column=1, row=1)
+
+######## window start ########
+update()
 root.mainloop()
